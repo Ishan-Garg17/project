ae6e4be6c80c83367a8727d7ece381aa6f2fed11
Ishan-Garg17 <ishangarg17@gmail.com>
2022-04-11 19:59:35 +0530

    .

---
+++ src/Containers/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+        {/* 
+        <nav/>
+        <Form/> 
+        <Notes/>
+        
+        */}
+
+    </div>
+  );
+}
+
+export default App;
