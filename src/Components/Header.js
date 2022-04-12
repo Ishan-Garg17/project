847457a9e907ba15580555d08622b64cc5d88529
Ishan-Garg17 <ishangarg17@gmail.com>
2022-04-12 17:16:02 +0530

    edits

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -4,12 +4,14 @@ const Header = ()=>{
     return(
         <div className="wrapper">
                 <div className='content'>
-                <span className='heading1'> The future </span>
-                <br/>
-                <span> of health </span>
-                <br/>
-                <span className='heading3'><em>&#8212;is clear</em></span>
-                <button className='btn'>Shop All</button>
+                    <div className='text'>
+                    <span className='heading1'> The future </span>
+                    <br/>
+                    <span> of health </span>
+                    <br/>
+                    <span className='heading3'><em>&#8212;is clear</em></span>
+                    <button className='btn'>Shop All</button>
+                    </div>
                 </div>
         </div>
     )
